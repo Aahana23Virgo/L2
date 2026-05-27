f584463e34f12ee25d943d172bfab168062c0319
Aahana23Virgo <aahana.august@gmail.com>
2026-05-27 15:37:02 +0530

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,14 @@
+def ONSquareTime(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end=" ")
+            iteration+=1
+        print("")
+    print("\nWhen n is ",n,"Iteration = ",iteration,"\n")
+
+ONSquareTime(5)
+ONSquareTime(15)
+
+print("\nWith every 'n' the time taken equals n*n")
+print("O(n*n)")
